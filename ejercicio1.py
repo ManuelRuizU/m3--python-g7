46fcfc1add9f681f7c883e2fa60989f3dc56567e
Manuel Ruiz U <mruiz.u@icloud.com>
2024-03-12 11:02:11 -0300

    mi primer python

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+echo "# m3--python-g7" >> README.md
+git init
+git add .
+git commit -m "mi primer python"
+git branch -M main
+git remote add origin https://github.com/ManuelRuizU/m3--python-g7.git
+git push -u origin main
+
+
